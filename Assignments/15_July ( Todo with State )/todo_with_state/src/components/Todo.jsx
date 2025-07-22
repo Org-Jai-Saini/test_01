d22295abaa5ca8e214117e4b0b98f2c9195aae6b
Org-Jai-Saini <jaisaini6167@gmail.com>
2025-07-22 09:07:50 +0530

    Assignment Added

--- Assignments/15_July ( Todo with State )/todo_with_state/src/components/Todo.jsx
+++ Assignments/15_July ( Todo with State )/todo_with_state/src/components/Todo.jsx
@@ -16,8 +16,14 @@ function ToDo() {
       setBoolean()
     }
 
+    function changeStatus(e, i){
+      let newTodo = [...todo]
+      newTodo[i].isComplete = e.target.checked;
+      setTodo(newTodo)
+    }
+
     return(
-      <div>
+      <div className="container">
         <input
         onChange={(e)=>setText(e.target.value)}
         value={text}
@@ -30,7 +36,7 @@ function ToDo() {
             <option value="true">True</option>
             <option value="false">False</option>
         </select>
-        <button onClick={addTask}>Add Task</button>
+        <button onClick={addTask}>Add Todo</button>
 
         <ul style={{
           listStyle : "none"
@@ -39,6 +45,11 @@ function ToDo() {
             return (
             
               <li key={i}>
+                <input 
+                onChange={(e)=>changeStatus(e, i)}
+                type="checkbox"
+                checked={v.isComplete}
+                />
                 <span
                 style={{
                   textDecoration: v.isComplete? "line-through" : "none",
